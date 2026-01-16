d17d6e30da32d7627dc0fabf9bc4ba45236d6e30
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2026-01-16 14:32:01 +0100

    feat/refacto-notification-center (#1)
    * refactor: remove unused component tests and improve notification handling via an abstract NotificationCreator that delegates the creation of the concrete sender to a factory : NotificationFactory that is an injectable service
    
    * refactor: update NotificationSender interface to return success status and delivery info, enhance notification handling in NotificationCenterComponent

--- src/app/components/notification-center.component.spec.ts
+++ src/app/components/notification-center.component.spec.ts
@@ -1,50 +1,6 @@
-// report-generator.component.spec.ts
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReportGeneratorComponent } from './report-generator.component';
-
-describe('ReportGeneratorComponent', () => {
-  let component: ReportGeneratorComponent;
-  let fixture: ComponentFixture<ReportGeneratorComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ReportGeneratorComponent]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ReportGeneratorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should generate sales report', () => {
-    component.selectedReportType = 'sales';
-    component.selectedFormat = 'pdf';
-    component.generateReport();
-    
-    expect(component.generatedReport).toBeTruthy();
-    expect(component.generatedReport?.title).toContain('Ventes');
-  });
-
-  it('should generate different formats', () => {
-    component.selectedReportType = 'sales';
-    
-    component.selectedFormat = 'pdf';
-    component.generateReport();
-    expect(component.generatedReport?.footer).toContain('PDF');
-    
-    component.selectedFormat = 'excel';
-    component.generateReport();
-    expect(component.generatedReport?.footer).toContain('Excel');
-  });
-});
-
 // notification-center.component.spec.ts
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NotificationCenterComponent } from './notification-center.component';
+import { NotificationCenterComponent, NotificationFactory } from './notification-center.component';
 
 describe('NotificationCenterComponent', () => {
   let component: NotificationCenterComponent;
@@ -52,7 +8,8 @@ describe('NotificationCenterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotificationCenterComponent]
+      imports: [NotificationCenterComponent],
+      providers: [NotificationFactory]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationCenterComponent);
@@ -60,63 +17,22 @@ describe('NotificationCenterComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    component.message = 'Test message';
+    component.selectedType = 'email';
+    component.sendNotification();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should send notification', () => {
-    component.message = 'Test message';
-    component.selectedType = 'email';
-    component.sendNotification();
-    
     expect(component.notifications.length).toBe(1);
     expect(component.notifications[0].type).toBe('email');
   });
 
   it('should get correct icon', () => {
-    expect(component.getIcon('email')).toBe('📧');
-    expect(component.getIcon('sms')).toBe('📱');
-    expect(component.getIcon('push')).toBe('🔔');
-  });
-});
-
-// dynamic-form.component.spec.ts
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DynamicFormComponent } from './dynamic-form.component';
-
-describe('DynamicFormComponent', () => {
-  let component: DynamicFormComponent;
-  let fixture: ComponentFixture<DynamicFormComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DynamicFormComponent]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(DynamicFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should add field', () => {
-    component.fieldLabel = 'Test Field';
-    component.selectedFieldType = 'text';
-    component.addField();
-    
-    expect(component.formFields.length).toBe(1);
-    expect(component.formFields[0].label).toBe('Test Field');
-  });
-
-  it('should remove field', () => {
-    component.fieldLabel = 'Test';
-    component.addField();
-    const fieldId = component.formFields[0].id;
-    
-    component.removeField(fieldId);
-    expect(component.formFields.length).toBe(0);
+    expect(component.notifications[0].icon).toBe('📧')
   });
 });
